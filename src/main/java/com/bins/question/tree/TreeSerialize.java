9e43310f286d6c0318857bb66cd305a6c4d0cf05
LEEBIN <199900libin@163.com>
2020-06-16 11:40:19 +0800

    --完成了今日的leetcode每日刷题

--- src/main/java/com/bins/question/tree/TreeSerialize.java
+++ src/main/java/com/bins/question/tree/TreeSerialize.java
@@ -1,17 +1,20 @@
 package com.bins.question.tree;
 
 
+import javax.swing.tree.TreeNode;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
  * @author leo-bin
  * @date 2020/4/15 13:28
  * @apiNote 序列化二叉树
- * 实现两个函数，分别用来序列化和反序列化二叉树
+ * 来源：leetcode-297
+ * 链接：https://leetcode-cn.com/problems/serialize-and-deserialize-binary-tree/
  */
 public class TreeSerialize {
-
 
     /**
      * 二叉树节点
@@ -34,17 +37,18 @@ public class TreeSerialize {
      * 示例:
      * 你可以将以下二叉树：
      * <p>
-     * 1
-     * / \
-     * 2   3
-     * / \
-     * 4   5
+     * *    1
+     * *   / \
+     * *  2   3
+     * *     / \
+     * *    4   5
      * 序列化为:
      * data="[1,2,3,null,null,4,5]"
      *
      * @apiNote 思路：
      * 1.依旧是二叉树的层次遍历
-     * 2.
+     * 2.时间复杂度：O(n)
+     * 3.空间复杂度：O(n)
      */
     public static String serialize(TreeNode root) {
         if (root == null) {
@@ -54,26 +58,24 @@ public class TreeSerialize {
         TreeNode current;
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-        result.append("[").append(root.data + ",");
+        result.append(root.data).append(",");
         while (!queue.isEmpty()) {
             current = queue.poll();
             //左子树
             if (current.left != null) {
                 queue.offer(current.left);
-                result.append(current.left.data + ",");
+                result.append(current.left.data).append(",");
             } else {
                 result.append("null,");
             }
             //右子树
             if (current.right != null) {
                 queue.offer(current.right);
-                result.append(current.right.data + ",");
+                result.append(current.right.data).append(",");
             } else {
                 result.append("null,");
             }
         }
-        result.deleteCharAt(result.length() - 1);
-        result.append("]");
         return result.toString();
     }
 
@@ -85,15 +87,15 @@ public class TreeSerialize {
      *
      * @apiNote 思路：
      * 1.还是层次遍历的思想
+     * 2.时间复杂度：O(n)
+     * 3.空间复杂度：O(n)
      */
     public static TreeNode deSerialize(String data) {
-        int len = data.length();
-        if (len == 0) {
+        if (data.length() == 0) {
             return null;
         }
-        String[] strs = data.replace("[", "").replace("]", "").split(",");
+        String[] strs = data.split(",");
         TreeNode root = new TreeNode(Integer.valueOf(strs[0]));
-        //节点个数统计
         int count = 1;
         TreeNode current;
         Queue<TreeNode> queue = new LinkedList<>();
@@ -112,6 +114,68 @@ public class TreeSerialize {
             count++;
         }
         return root;
+    }
+
+    /**
+     * 序列化，dfs解法
+     *
+     * @apiNote 思路：
+     * 1.dfs+递归
+     * 2.时间复杂度：O(n)
+     * 3.空间复杂度：O(n)
+     */
+    public static String serializeV2(TreeNode root) {
+        StringBuilder result = serializeHelper(root, new StringBuilder());
+        return result.toString();
+    }
+
+    /**
+     * dfs
+     */
+    public static StringBuilder serializeHelper(TreeNode root, StringBuilder builder) {
+        //1.递归结束条件
+        if (root == null) {
+            builder.append("null,");
+            return builder;
+        } else {
+            builder.append(root.data).append(",");
+            //2.左右继续递归
+            serializeHelper(root.left, builder);
+            serializeHelper(root.right, builder);
+        }
+        return builder;
+    }
+
+
+    /**
+     * 反序列化，dfs解法
+     *
+     * @apiNote 思路：
+     * 1.dfs+递归
+     * 2.时间复杂度：O(n)
+     * 3.空间复杂度：O(n)
+     */
+    public static TreeNode deSerializeV2(String data) {
+        String[] strs = data.split(",");
+        LinkedList<String> list = new LinkedList<>(Arrays.asList(strs));
+        return deSerializeHelper(list);
+    }
+
+
+    /**
+     * dfs
+     */
+    public static TreeNode deSerializeHelper(LinkedList<String> list) {
+        //1.递归结束条件
+        if ("null".equals(list.get(0))) {
+            list.remove(0);
+            return null;
+        }
+        TreeNode cur = new TreeNode(Integer.valueOf(list.get(0)));
+        list.remove(0);
+        cur.left = deSerializeHelper(list);
+        cur.right = deSerializeHelper(list);
+        return cur;
     }
 
 
