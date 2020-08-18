d03b6fca29a31fe1885968f6e84e219a030e17d3
LEEBIN <199900libin@163.com>
2020-08-18 18:14:40 +0800

    --复盘了岛屿的数量问题，回溯+dfs模板。

--- src/main/java/com/bins/question/backtrace/NumOfIslands.java
+++ src/main/java/com/bins/question/backtrace/NumOfIslands.java
@@ -4,9 +4,10 @@ package com.bins.question.backtrace;
  * @author leo-bin
  * @date 2020/4/20 12:08
  * @apiNote 岛屿的数量
+ * 来源：leetcode-200
+ * 链接：https://leetcode-cn.com/problems/number-of-islands/
  */
 public class NumOfIslands {
-
 
     /**
      * 题目描述：
@@ -32,7 +33,7 @@ public class NumOfIslands {
      * 解释: 每座岛屿只能由水平或竖直方向上相邻的陆地连接而成。
      *
      * @apiNote 思路：
-     * 1.这道题本质上是一个深度优先搜索的问题
+     * 1.这道题本质上是一个深度优先搜索dfs的问题
      * 2.我们从第一个位置开始，只需要判断下它的上下左右四个位置是否是1，如果是1，标记为2
      * 3.然后递归的从这四个位置依次判断就行
      * 4.递归结束条件需要判断下是否越界，是否是1
@@ -52,25 +53,20 @@ public class NumOfIslands {
         return count;
     }
 
-
-
     /**
      * 感染函数
      */
     public static void infect(char[][] grid, int i, int j) {
-        //判断是否越界或者是不是满足题目的条件
+        //递归结束条件
         if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
             return;
         }
         //感染当前的位置
         grid[i][j] = '2';
-        //往上感染
+        //从当前位置的上下左右尝试感染
         infect(grid, i - 1, j);
-        //往下感染
         infect(grid, i + 1, j);
-        //往左感染
         infect(grid, i, j - 1);
-        //往右感染
         infect(grid, i, j + 1);
     }
 
