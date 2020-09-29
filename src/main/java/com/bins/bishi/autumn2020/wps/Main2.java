862169c91a80a04a1ce1a6fd105c616ec0257510
LEEBIN <199900libin@163.com>
2020-09-29 21:12:06 +0800

    --考了金山wps，还不错，依旧保持同样的心态吧。

---
+++ src/main/java/com/bins/bishi/autumn2020/wps/Main2.java
@@ -0,0 +1,16 @@
+package com.bins.bishi.autumn2020.wps;
+
+import java.util.Scanner;
+
+/**
+ * @author leo-bin
+ * @date 2020/9/29 18:56
+ * @apiNote
+ */
+public class Main2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
